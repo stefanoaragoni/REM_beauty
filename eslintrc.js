138dac43d3e40c928fc43e4b63ed41085a26f04a
stefanoaragoni <69864405+stefanoaragoni@users.noreply.github.com>
2022-05-27 20:28:08 -0600

    babel + eslintrc files

---
+++ eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    extends: [
+      'plugin:react/recommended',
+      'airbnb',
+    ],
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 'latest',
+    },
+    plugins: [
+      'react',
+    ],
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-filename-extension": "off",
+      "no-undef": "off",
+      "semi": [2, "never"]
+    },
+  };
+  
